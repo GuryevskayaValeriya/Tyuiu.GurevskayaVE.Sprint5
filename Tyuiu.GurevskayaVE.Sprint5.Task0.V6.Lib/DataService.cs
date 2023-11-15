da7b44554aa86c78442ba4ac79e4956bf4fa8510
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-11-15 10:58:31 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GurevskayaVE.Sprint5.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.GurevskayaVE.Sprint5.Task0.V6.Lib
+{
+    public class DataService : ISprint5Task0V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double res = x/(Math.Sqrt(Math.Pow(x,2)+x));
+            res = Math.Round(res,3);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.GurevskayaVE.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GurevskayaVE.Sprint5.Task0.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.GurevskayaVE.Sprint5.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test0()
+        {
+            string path = @"C:\Users\ВАЛЕРИЯ 2004\source\repos\Tyuiu.GurevskayaVE.Sprint5\Tyuiu.GurevskayaVE.Sprint5.Task0.V6\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+    }
+}
