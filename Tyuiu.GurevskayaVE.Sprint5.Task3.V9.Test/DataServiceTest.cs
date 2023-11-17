18f074c7dce32199a5c4eccabc483d8457b80a1e
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-11-17 18:57:29 +0500

    task3

--- Tyuiu.GurevskayaVE.Sprint5.Task3.V9.Test/DataServiceTest.cs
+++ Tyuiu.GurevskayaVE.Sprint5.Task3.V9.Test/DataServiceTest.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
-using Tyuiu.GurevskayaVE.Sprint5.Task2.V20.Lib;
+using Tyuiu.GurevskayaVE.Sprint5.Task3.V9.Lib;
 
-namespace Tyuiu.GurevskayaVE.Sprint5.Task2.V20.Test
+namespace Tyuiu.GurevskayaVE.Sprint5.Task3.V9.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void Test2()
+        public void Test3()
         {
-            string path = @"C:\Users\ВАЛЕРИЯ 2004\source\repos\Tyuiu.GurevskayaVE.Sprint5\Tyuiu.GurevskayaVE.Sprint5.Task2.V20\bin\Debug\OutPutFileTask2.csv";
+            string path = @"C:\Users\ВАЛЕРИЯ 2004\source\repos\Tyuiu.GurevskayaVE.Sprint5\Tyuiu.GurevskayaVE.Sprint5.Task3.V9\bin\Debug\OutPutFileTask3.bin";
             FileInfo fileinfo = new FileInfo(path);
             bool fileex = fileinfo.Exists;
             bool wait = true;
