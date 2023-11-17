f2ee606fab9c1e7b938dd85e0abe79aa42e2203f
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-11-17 18:57:40 +0500

    task2

--- Tyuiu.GurevskayaVE.Sprint5.Task2.V20.Test/DataServiceTest.cs
+++ Tyuiu.GurevskayaVE.Sprint5.Task2.V20.Test/DataServiceTest.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.GurevskayaVE.Sprint5.Task2.V20.Test
             string path = @"C:\Users\ВАЛЕРИЯ 2004\source\repos\Tyuiu.GurevskayaVE.Sprint5\Tyuiu.GurevskayaVE.Sprint5.Task2.V20\bin\Debug\OutPutFileTask2.csv";
             FileInfo fileinfo = new FileInfo(path);
             bool fileex = fileinfo.Exists;
-            bool wait = true;
+            bool wait = true; 
             Assert.AreEqual(wait, fileex);
         }
     }
